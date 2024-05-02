94d93bc7c9a5ee63759038c4e6efd0df780ab9b6
Shawn Toubeau <shawntoubeau@gmail.com>
2024-05-01 20:19:58 -0400

    disable speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <SpeedInsights />
+          {/* <SpeedInsights /> */}
           <Analytics />
         </ThemeProvider>
       </body>
