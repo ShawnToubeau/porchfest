43e3e8cb11893374767c5bf8172bfe132284f0fc
Shawn Toubeau <shawntoubeau@gmail.com>
2024-05-01 20:21:54 -0400

    disable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         >
           {children}
           {/* <SpeedInsights /> */}
-          <Analytics />
+          {/* <Analytics /> */}
         </ThemeProvider>
       </body>
     </html>
