d2760f9be9b20030c80ad21fd5a76ba81c332d14
Shawn Toubeau <shawntoubeau@gmail.com>
2025-05-07 21:28:02 -0400

    increase svg size

--- src/app/event-map.tsx
+++ src/app/event-map.tsx
@@ -283,7 +283,7 @@ export default function EventMap() {
       }
 
       // Create an image from SVG
-      const svgImage = new Image(36, 36);
+      const svgImage = new Image(42, 42);
       svgImage.onload = () => {
           map.addImage('bathroom-icon', svgImage)
       }
