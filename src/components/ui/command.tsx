b35f9ddeed3eaf05f0f4482d8d6858c9d3b7532a
Shawn Toubeau <shawntoubeau@gmail.com>
2025-05-08 22:42:58 -0400

    increase padding of search clear

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -56,7 +56,7 @@ const CommandInput = React.forwardRef<
     />
 
     {onClear && (
-      <div className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" onClick={onClear}>
+      <div className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground cursor-pointer p-1" onClick={onClear}>
         <X className="h-4 w-4 shrink-0" />
         <span className="sr-only">Close</span>
       </div>
